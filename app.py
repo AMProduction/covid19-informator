44da407bb5785d70c050dcb706a91adf44e5bfa1
Andrii Malchyk <snooki17@gmail.com>
2021-12-26 19:49:44 +0200

    Version v.1.0
    Signed-off-by: Andrii Malchyk <snooki17@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from datetime import date, timedelta
-
 from flask import Flask, render_template, request
 
 import src.etl as etl
@@ -13,18 +11,17 @@ def show_historical_data():
         search_date = request.form['datepicker']
         region_code = request.form['region']
         result_json = etl.etl(search_date, region_code)
+        if result_json['Province'] == 'all':
+            search_date = etl.get_yesterday_date()
+            region_code = 'all'
     return render_template('index.html', data=result_json, date=search_date, region=region_code)
 
 
 @app.route('/')
 def home():
     # Initial loading of the main page
-    today = date.today()
-    # Get and format yesterday date
-    yesterday = today - timedelta(days=1)
-    yesterday = yesterday.strftime('%m-%d-%Y')
-    result_json = etl.etl(yesterday, 'all')
-    return render_template('index.html', data=result_json, date=yesterday, region='Whole country')
+    result_json = etl.etl(etl.get_yesterday_date(), 'all')
+    return render_template('index.html', data=result_json, date=etl.get_yesterday_date(), region='Whole country')
 
 
 if __name__ == '__main__':
--- src/etl.py
+++ src/etl.py
@@ -8,6 +8,13 @@ import pandas as pd
 import requests
 
 
+def get_yesterday_date():
+    today = date.today()
+    # Get and format yesterday date
+    yesterday = today - timedelta(days=1)
+    return yesterday.strftime('%m-%d-%Y')
+
+
 def get_daily_dataset_url(date):
     # Check date format (only MM-DD-YYYY is acceptable)
     if re.search("^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-20(19|20|21|22)$", date) is None:
@@ -32,33 +39,37 @@ def clean_dataset(dataset_url):
     daily_dataset = requests.get(dataset_url).url
     # Load CSV
     df_all_countries = pd.read_csv(daily_dataset)
-    # Select Ukraine only
-    ukraine_dataset = df_all_countries[df_all_countries["Country_Region"] == "Ukraine"]
-    # Clean the dataset
-    ukraine_dataset = ukraine_dataset.drop(["FIPS", "Admin2", "Last_Update", "Lat", "Long_", "Combined_Key"], axis=1)
-    ukraine_dataset.fillna(0, inplace=True)
-    ukraine_dataset[["Confirmed", "Deaths", "Recovered", "Active", "Incident_Rate", "Case_Fatality_Ratio"]] = \
-        ukraine_dataset[["Confirmed", "Deaths", "Recovered", "Active", "Incident_Rate", "Case_Fatality_Ratio"]].apply(
-            pd.to_numeric, errors='coerce')
-    # Some calculations
-    confirmed_total = ukraine_dataset['Confirmed'].sum()
-    deaths_total = ukraine_dataset['Deaths'].sum()
-    incident_rate_median = ukraine_dataset['Incident_Rate'].median()
-    case_fatality_ratio_median = ukraine_dataset['Case_Fatality_Ratio'].median()
-    # Total row
-    total_data = {'Province_State': ['all'],
-                  'Country_Region': ['Ukraine'],
-                  'Confirmed': [confirmed_total],
-                  'Deaths': [deaths_total],
-                  'Recovered': [0],
-                  'Active': [0],
-                  'Incident_Rate': [incident_rate_median],
-                  'Case_Fatality_Ratio': [case_fatality_ratio_median]
-                  }
-    total_row = pd.DataFrame(total_data, index=['0'])
-    # Add the new total row
-    ukraine_dataset = ukraine_dataset.append(total_row)
-    return ukraine_dataset
+    # Check .csv formatting (There is an old file format)
+    if "Country_Region" in df_all_countries:
+        # Select Ukraine only
+        ukraine_dataset = df_all_countries[df_all_countries["Country_Region"] == "Ukraine"]
+        # Clean the dataset
+        ukraine_dataset = ukraine_dataset.drop(["FIPS", "Admin2", "Last_Update", "Lat", "Long_", "Combined_Key"], axis=1)
+        ukraine_dataset.fillna(0, inplace=True)
+        ukraine_dataset[["Confirmed", "Deaths", "Recovered", "Active", "Incident_Rate", "Case_Fatality_Ratio"]] = \
+            ukraine_dataset[["Confirmed", "Deaths", "Recovered", "Active", "Incident_Rate", "Case_Fatality_Ratio"]].apply(
+                pd.to_numeric, errors='coerce')
+        # Some calculations
+        confirmed_total = ukraine_dataset['Confirmed'].sum()
+        deaths_total = ukraine_dataset['Deaths'].sum()
+        incident_rate_median = ukraine_dataset['Incident_Rate'].median()
+        case_fatality_ratio_median = ukraine_dataset['Case_Fatality_Ratio'].median()
+        # Total row
+        total_data = {'Province_State': ['all'],
+                      'Country_Region': ['Ukraine'],
+                      'Confirmed': [confirmed_total],
+                      'Deaths': [deaths_total],
+                      'Recovered': [0],
+                      'Active': [0],
+                      'Incident_Rate': [incident_rate_median],
+                      'Case_Fatality_Ratio': [case_fatality_ratio_median]
+                      }
+        total_row = pd.DataFrame(total_data, index=['0'])
+        # Add the new total row
+        ukraine_dataset = ukraine_dataset.append(total_row)
+        return ukraine_dataset
+    else:
+        return None
 
 
 def fill_the_table(date, dataset):
@@ -123,13 +134,13 @@ def etl(search_date, region_code):
         # If YES - download and process
         if is_source_exists(df_url):
             result_dataset = clean_dataset(df_url)
-            fill_the_table(search_date, result_dataset)
-            result_json = get_info_from_db(search_date, region_code)
+            # Check format file check result
+            if result_dataset is not None:
+                fill_the_table(search_date, result_dataset)
+                result_json = get_info_from_db(search_date, region_code)
+            else:
+                result_json = get_info_from_db(get_yesterday_date(), 'all')
         # If NO - get yesterday data
         else:
-            today = date.today()
-            # Get and format yesterday date
-            yesterday = today - timedelta(days=1)
-            yesterday = yesterday.strftime('%m-%d-%Y')
-            result_json = get_info_from_db(yesterday, 'all')
+            result_json = get_info_from_db(get_yesterday_date(), 'all')
     return result_json
